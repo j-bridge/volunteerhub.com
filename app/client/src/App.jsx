5f37100e6a2b712f1f476feefa0948db546efbd5
Cameron Bachour <cbachour2022@fau.edu>
2025-11-10 11:58:07 -0500

    Verified routes and navbar navigation before assigning Nadia tasks

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -3,6 +3,10 @@ import RootLayout from "./layouts/RootLayout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Volunteer from "./pages/Volunteer"; // acts as the volunteer dashboard
+import OrgDashboard from "./pages/OrgDashboard";
 
 export default function App() {
   return (
@@ -15,12 +19,17 @@ export default function App() {
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
 
-        {/* Future pages can go here */}
-        {/* <Route path="opportunities" element={<Opportunities />} /> */}
-        {/* <Route path="about" element={<About />} /> */}
-        {/* <Route path="contact" element={<Contact />} /> */}
+        {/* Volunteer experience & org dashboard */}
+        <Route path="dashboard" element={<Volunteer />} />
+        <Route path="opportunities" element={<Volunteer />} />
+        <Route path="org/dashboard" element={<OrgDashboard />} />
+
+        {/* Informational pages */}
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
   );
 }
+
 
---
+++ app/client/src/pages/OrgDashboard.jsx
@@ -0,0 +1,3 @@
+export default function OrgDashboard() {
+  return <h1>Organization Dashboard (Placeholder)</h1>;
+}
